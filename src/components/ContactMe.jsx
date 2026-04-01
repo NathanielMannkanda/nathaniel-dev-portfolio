1f4dacd33e93bc5a66420640e35d06585cce3998
Natheniel Mankanda <nathanielmankanda8@gmail.com>
2026-04-01 15:28:34 +0200

    updated phone number

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -20,7 +20,7 @@ export const ContactMe = () => {
         </div>
 
         <div className="tooltip mr-2">
-          <a href="https://wa.me/24632402210" target="_blank">
+          <a href="https://wa.me/27632402210" target="_blank">
             <img 
               className='h-5 w-5 invert' 
               src="./imgs/whatsapp.png" 
