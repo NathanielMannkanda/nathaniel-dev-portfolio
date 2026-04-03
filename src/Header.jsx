18f5d3b32a028204e8329f904f30462996dadca7
Natheniel Mankanda <nathanielmankanda8@gmail.com>
2026-04-03 19:18:51 +0200

    updated CV link

--- src/Header.jsx
+++ src/Header.jsx
@@ -72,7 +72,7 @@ export const Header = () => {
           className="resume-button border"
 
           onClick={() => {
-            window.open("/Nathaniel-Mankanda-Cv.pdf", "_blank"), console.log("THX :) ... CV opened")
+            window.open("/Nathaniel-Mankanda-Cv-2.pdf", "_blank"), console.log("THX :) ... CV opened")
           }}
 
           onMouseEnter={handleEnter}
